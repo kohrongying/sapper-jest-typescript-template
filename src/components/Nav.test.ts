835fe1e7b6efe2f8ebff41ea51de7b90ec8852a8
rongying <rongying.koh@thoughtworks.com>
2020-09-05 16:35:15 +0800

    Simulate failed test on github action

--- src/components/Nav.test.ts
+++ src/components/Nav.test.ts
@@ -5,7 +5,7 @@ test("renders navbar links", () => {
   const { getByText } = render(Nav);
   const blogElement = getByText(/blog/i);
   const homeElement = getByText(/home/i);
-  const aboutElement = getByText(/about/i);
+  const aboutElement = getByText(/rongying/i);
 
   expect(blogElement).toBeInTheDocument();
   expect(homeElement).toBeInTheDocument();
