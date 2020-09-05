cb34be4deef72c40d379aa8a00a38c2ed986ec1a
rongying <rongying.koh@thoughtworks.com>
2020-09-05 16:36:55 +0800

    Fix test

--- src/components/Nav.test.ts
+++ src/components/Nav.test.ts
@@ -5,7 +5,7 @@ test("renders navbar links", () => {
   const { getByText } = render(Nav);
   const blogElement = getByText(/blog/i);
   const homeElement = getByText(/home/i);
-  const aboutElement = getByText(/rongying/i);
+  const aboutElement = getByText(/about/i);
 
   expect(blogElement).toBeInTheDocument();
   expect(homeElement).toBeInTheDocument();
